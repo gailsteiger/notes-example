62fdbe16d774c8d8acb07db208fbe343fab56607
gail <gail@gailsteiger.com>
2013-08-03 23:49:52 +0300

    namespace tests

--- test/js/spec/namespace.spec.js
+++ test/js/spec/namespace.spec.js
@@ -9,5 +9,14 @@ describe('Namespoce', function (){
     it("provides the 'App' object", function (){
         // Expect exists and is an object.
         expect(App).to.be.an("object");
-    })
-})
+
+        // Expect all namespace properties are present.
+        expect(App).to.include.keys(
+            "Config", "Collections", "Models",
+            "Routers", "Templates", "Views"
+        );
+    });
+    it("provides the 'app' object", function (){
+        expect(app).to.be.an("object");
+    });
+});
