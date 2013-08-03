5af0247a4acd3ac11a5dfc78801f2380529f5ed6
gail <gail@gailsteiger.com>
2013-08-03 23:37:45 +0300

    initial setup

--- app/js/app/namespace.spec.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created with JetBrains WebStorm.
- * User: gs
- * Date: 03/08/13
- * Time: 21:32
- * To change this template use File | Settings | File Templates.
- */
---
+++ test/js/spec/namespace.spec.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: gs
+ * Date: 03/08/13
+ * Time: 21:32
+ * To change this template use File | Settings | File Templates.
+ */
+describe('Namespoce', function (){
+    it("provides the 'App' object", function (){
+        // Expect exists and is an object.
+        expect(App).to.be.an("object");
+    })
+})
